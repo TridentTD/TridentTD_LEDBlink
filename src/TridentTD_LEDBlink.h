585de8223841052c18f0357eada14b356e066d1d
TridentTD <prote2000@gmail.com>
2018-11-10 11:42:31 +0700

    Update src/TridentTD_LEDBlink.h

--- src/TridentTD_LEDBlink.h
+++ src/TridentTD_LEDBlink.h
@@ -4,15 +4,15 @@
 #include <Arduino.h>
 
 #ifndef TURN_ON
-#define TURN_ON 	HIGH
+#define TURN_ON   HIGH
 #endif
 
 class TridentTD_LEDBlink {
   public:
-    TridentTD_LEDBlink(uint8_t pin) :_pin(pin) 	{ pinMode( _pin, OUTPUT);}
-    void blink(uint32_t ms = 1000)  						{if( millis() - _timer > ms ){ static uint8_t _state; digitalWrite(_pin, _state = !_state); _timer = millis(); }}
-    void off()    															{ digitalWrite( _pin, !TURN_ON); }
-    void on()     															{ digitalWrite( _pin,  TURN_ON); }
+    TridentTD_LEDBlink(uint8_t pin) :_pin(pin)  { pinMode( _pin, OUTPUT);}
+    void blink(uint32_t ms = 1000)              {if( millis() - _timer > ms ){ static uint8_t _state; digitalWrite(_pin, _state = !_state); _timer = millis(); }}
+    void off()                                  { digitalWrite( _pin, !TURN_ON); }
+    void on()                                   { digitalWrite( _pin,  TURN_ON); }
   private:
     uint8_t _pin;
     uint32_t _timer;
